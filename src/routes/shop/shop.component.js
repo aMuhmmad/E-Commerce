cb543850262a110385a838489691430e4385a7a4
a <email@example>
2023-08-12 09:29:06 +0300

    added redux thunk

--- src/routes/shop/shop.component.js
+++ src/routes/shop/shop.component.js
@@ -4,8 +4,7 @@ import CategoriesPreview from '../categories-preview/categories-preview.componen
 
 import Category from '../category/category.component'
 import { useEffect } from 'react';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories} from '../../store/categories/category.action';
+import { fetchCategoriesAsync} from '../../store/categories/category.action';
 import { useDispatch } from 'react-redux';
 
 const Shop = () => {
@@ -13,12 +12,7 @@ const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categoriesArray = await getCategoriesAndDocuments('categories');
-            dispatch(setCategories(categoriesArray));
-        };
-
-        getCategoriesMap();
+        dispatch(fetchCategoriesAsync());
     }, []);
 
     return (
--- src/store/store.js
+++ src/store/store.js
@@ -4,8 +4,9 @@ import { rootReducer } from "./root-reducer";
 
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
+const middleWares = [process.env.NODE_ENV !== 'production' && logger,thunk].filter(
     Boolean
 );
 
